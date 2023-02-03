1e359fdb930029496799f84bda7cef52f488f543
SamHuggins <shuggins65@gmail.com>
2023-02-02 18:27:10 -0600

    Added code to the JS file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-ECHO is on.
+
+//sting example
+var firstName = "Sam";
+var lastName = "Huggins";
+
+//boolian example
+var isRaining = true;
+
+//array example [string, number, boolian]
+var petFacts = [ 'Frumpkin', 1, false]
+
+
+
+console.log("Fullname: " + firstName + " " + lastName);
+console.log(isRaining);
+console.log(petFacts);
+
+
+//testing the difference between == and ===
+var num = 1;
+var num2 = "1";
+var num3 = 2;
+
+console.log('num == num2 ', num == num2)
+console.log('num === num2 ', num === num2)
+
